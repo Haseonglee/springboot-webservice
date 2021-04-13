e166721f85dbbf19dad82dd5473463c7177e1df8
Haseonglee <hhh4389@naver.com>
2021-04-13 23:05:42 +0900

    커밋

---
+++ src/main/java/com/seong/springboot/web/PostsApiController.java
@@ -0,0 +1,33 @@
+package com.seong.springboot.web;
+
+import com.seong.springboot.service.posts.PostService;
+import com.seong.springboot.web.dto.PostsResponseDto;
+import com.seong.springboot.web.dto.PostsSaveRequestDto;
+import com.seong.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostService postService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+
+        return postService.save(requestDto);
+    }
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody
+            PostsUpdateRequestDto requestDto) {
+
+        return postService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+
+        return postService.findById(id);
+    }
+}
